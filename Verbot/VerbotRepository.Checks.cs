b9df56553777706c350df8e6f23532aad3abbbd0
Ron MacNeil <macro@hotmail.com>
2020-05-30 19:15:57 +1000

    Refactor and simplify missing release checks

--- Verbot/VerbotRepository.Checks.cs
+++ Verbot/VerbotRepository.Checks.cs
@@ -18,7 +18,7 @@ namespace Verbot
             CheckForMissingVersions();
             CheckForReleaseZero();
             CheckForMultipleReleasesFromSingleCommit();
-            CheckForMissingReleaseTags();
+            CheckForMissingReleases();
             CheckReleaseLineage();
             CheckReleaseSemverCommits();
         }
@@ -32,210 +32,10 @@ namespace Verbot
         }
 
 
-        void CheckForVersionLocations()
-        {
-            var locations = FindVersionLocations();
-
-            if (locations.Count == 0)
-            {
-                throw new UserException("No locations found in repository to record version");
-            }
-        }
-
-
-        void CheckForConflictingVersions()
-        {
-            var locations = FindVersionLocations();
-
-            var distinctVersions =
-                locations
-                    .Select(location => location.GetVersion())
-                    .Where(version => version != null)
-                    .Distinct()
-                    .ToList();
-
-            if (distinctVersions.Count == 1)
-            {
-                return;
-            }
-
-            Trace.TraceError("Conflicting versions found in repository:");
-            foreach (var location in locations)
-            {
-                var version = location.GetVersion() ?? "(none)";
-                var description = location.Description;
-                Trace.TraceError($"  {version} in {description}");
-            }
-            Trace.TraceError("Consider re-setting the correct current version using the 'set' command.");
-
-            throw new UserException("Conflicting versions found in repository");
-        }
-
-
-        void CheckForMissingVersions()
-        {
-            var locations = FindVersionLocations();
-            var missingLocations =
-                locations
-                    .Where(location => location.GetVersion() == null)
-                    .ToList();
-
-            if (missingLocations.Count == 0)
-            {
-                return;
-            }
-
-            Trace.TraceWarning($"Missing versions in some location(s) in the repository:");
-            foreach (var location in missingLocations)
-            {
-                Trace.TraceWarning($"  {location.Description}");
-            }
-            Trace.TraceWarning("Consider re-setting the current version using the 'set' command.");
-        }
-
-
         void CheckNoUncommittedChanges()
         {
             if (GitRepository.HasUncommittedChanges())
                 throw new UserException("Uncommitted changes in repository");
-        }
-
-
-        void CheckVersionHasNotBeenReleased()
-        {
-            var releaseVersion = ReadFromVersionLocations().Change(null, null, null, "", "");
-            if (GitRepository.GetTags().Any(t => t.Name == releaseVersion))
-                throw new UserException("Current version has already been released");
-        }
-
-
-        void CheckVersionIsMasterPrerelease()
-        {
-            var version = ReadFromVersionLocations();
-            if (version.Prerelease != "master")
-                throw new UserException("Expected current version to be a -master prerelease");
-        }
-
-
-        void CheckVersionIsReleaseOrMasterPrerelease()
-        {
-            var version = ReadFromVersionLocations();
-            if (!(version.Prerelease == "" || version.Prerelease == "master"))
-                throw new UserException("Expected current version to be a release or -master prerelease");
-        }
-
-
-        void CheckMasterBranchIsTrackingHighestVersion()
-        {
-            if (MasterBranches.Any(mb => mb.Name == "master") && MasterBranches.First().Name != "master")
-                throw new UserException("Expected master branch to be tracking the latest version");
-        }
-
-
-        void CheckOnCorrectMasterBranchForVersion()
-        {
-            var minorVersion = ReadFromVersionLocations().Change(null, null, 0, "", "");
-            var expectedCurrentBranch =
-                MasterBranches
-                    .Where(mb => mb.Version == minorVersion)
-                    .Select(mb => mb.Name)
-                    .SingleOrDefault();
-            if (expectedCurrentBranch == null)
-                throw new UserException("No master branch found for current version");
-            if (GitRepository.GetBranch() != expectedCurrentBranch)
-                throw new UserException("Expected to be on branch " + expectedCurrentBranch);
-        }
-
-
-        void CheckNotSkippingRelease(bool major, bool minor)
-        {
-            var patch = !(major || minor);
-            var version = ReadFromVersionLocations();
-            if (version.Prerelease != "master") return;
-            var patchString = FormattableString.Invariant($"{version.Major}.{version.Minor}.{version.Patch}");
-            var minorString = FormattableString.Invariant($"{version.Major}.{version.Minor}");
-            if (patch)
-                throw new UserException(FormattableString.Invariant(
-                    $"No need to increment patch when {patchString} hasn't been released yet"));
-            if (minor && version.Patch == 0)
-                throw new UserException(FormattableString.Invariant(
-                    $"No need to increment minor when {patchString} hasn't been released yet"));
-            if (major && version.Minor == 0 && version.Patch == 0)
-                throw new UserException(FormattableString.Invariant(
-                    $"No need to increment major when {minorString} hasn't been released yet"));
-        }
-
-
-        void CheckNotAdvancingToLatestVersionOnNonMasterBranch(SemVersion newVersion)
-        {
-            if (!MasterBranches.Any()) return;
-            var newMinorVersion = newVersion.Change(null, null, 0, "", "");
-            if (newMinorVersion > MasterBranches.First().Version && GitRepository.GetBranch() != "master")
-                throw new UserException("Must be on master branch to advance to latest version");
-        }
-
-
-        void CheckForIncorrectRemoteTags()
-        {
-            var verbotTagsWithRemote = FindReleaseTagsWithRemote();
-
-            var incorrectRemoteTags =
-                verbotTagsWithRemote
-                    .Where(t => t.RemoteTarget != null)
-                    .Where(t => t.RemoteTarget != t.Target)
-                    .ToList();
-
-            if (!incorrectRemoteTags.Any()) return;
-
-            foreach (var tag in incorrectRemoteTags)
-            {
-                Trace.TraceError($"Remote tag {tag.Name} at {tag.RemoteTarget} local {tag.Target}");
-            }
-
-            throw new UserException("Incorrect remote tag(s) found");
-        }
-
-
-        void CheckForRemoteBranchesAtUnknownCommits()
-        {
-            var verbotBranchesWithRemote = GetVerbotBranchesWithRemote();
-
-            var remoteBranchesAtUnknownCommits =
-                verbotBranchesWithRemote
-                    .Where(b => b.RemoteTarget != null)
-                    .Where(b => !GitRepository.Exists(b.RemoteTarget))
-                    .ToList();
-
-            if (!remoteBranchesAtUnknownCommits.Any()) return;
-
-            foreach (var branch in remoteBranchesAtUnknownCommits)
-            {
-                Trace.TraceError($"Remote branch {branch.Name} at unknown commit {branch.RemoteTarget}");
-            }
-
-            throw new UserException("Remote branch(es) at unknown commits");
-        }
-
-
-        void CheckForRemoteBranchesNotBehindLocal()
-        {
-            var verbotBranchesWithRemote = GetVerbotBranchesWithRemote();
-
-            var remoteBranchesNotBehindLocal =
-                verbotBranchesWithRemote
-                    .Where(b => b.RemoteTarget != null)
-                    .Where(b => !GitRepository.IsAncestor(b.RemoteTarget, b.Target))
-                    .ToList();
-
-            if (!remoteBranchesNotBehindLocal.Any()) return;
-
-            foreach (var branch in remoteBranchesNotBehindLocal)
-            {
-                Trace.TraceError(
-                    $"Remote branch {branch.Name} at {branch.RemoteTarget} not behind local at {branch.Target}");
-            }
-
-            throw new UserException("Remote branch(es) not behind local");
         }
 
 
@@ -254,11 +54,13 @@ namespace Verbot
 
             foreach (var releases in CommitReleaseLookup)
             {
-                if (releases.Count() <= 1) continue;
-                var sha1 = releases.Key.Sha1;
-                var releaseNames = string.Join(", ", releases.Select(t => t.Version));
-                Trace.TraceError($"Multiple releases on commit {sha1}: {releaseNames}");
-                passed = false;
+                if (releases.Count() > 1)
+                {
+                    var sha1 = releases.Key.Sha1;
+                    var releaseNames = string.Join(", ", releases.Select(t => t.Version));
+                    Trace.TraceError($"Multiple releases on commit {sha1}: {releaseNames}");
+                    passed = false;
+                }
             }
 
             if (!passed)
@@ -268,65 +70,81 @@ namespace Verbot
         }
 
 
-        void CheckForMissingReleaseTags()
+        void CheckForMissingReleases()
         {
-            if (!ReleasesDescending.Any()) return;
-
-            var allVersions = new HashSet<SemVersion>(ReleasesDescending.Select(r => r.Version));
-            var missingVersions = new List<SemVersion>();
-
-            var latestVersion = allVersions.Max();
-            for (var major = 1; major <= latestVersion.Major; major++)
-            {
-                var v = new SemVersion(major, 0, 0);
-                if (!allVersions.Contains(v))
-                {
-                    missingVersions.Add(v);
-                }
-            }
-
-            var latestMinorVersions =
-                allVersions
-                    .GroupBy(v => v.Change(minor: 0, patch: 0))
-                    .Select(g => g.Max())
-                    .OrderBy(v => v);
-            foreach (var latestMinorVersion in latestMinorVersions)
-            {
-                for (var minor = 1; minor <= latestMinorVersion.Minor; minor++)
-                {
-                    var v = latestMinorVersion.Change(minor: minor, patch: 0);
-                    if (!allVersions.Contains(v))
-                    {
-                        missingVersions.Add(v);
-                    }
-                }
-            }
-
-            var latestPatchVersions =
-                allVersions
-                    .GroupBy(v => v.Change(patch: 0))
-                    .Select(g => g.Max())
-                    .OrderBy(v => v);
-            foreach (var latestPatchVersion in latestPatchVersions)
-            {
-                for (var patch = 1; patch < latestPatchVersion.Patch; patch++)
-                {
-                    var v = latestPatchVersion.Change(patch: patch);
-                    if (!allVersions.Contains(v))
-                    {
-                        missingVersions.Add(v);
-                    }
-                }
-            }
+            var missingVersions =
+                Enumerable.Empty<SemVersion>()
+                    .Concat(CheckForMissingMajorReleases())
+                    .Concat(CheckForMissingMinorReleases())
+                    .Concat(CheckForMissingPatchReleases());
 
             if (missingVersions.Any())
             {
-                foreach (var missingVersion in missingVersions.OrderBy(v => v))
+                foreach (var version in missingVersions.OrderBy(v => v))
                 {
-                    Trace.TraceError($"Missing release {missingVersion}");
+                    Trace.TraceError($"Missing release {version}");
                 }
 
                 throw new UserException("Missing release(s)");
+            }
+        }
+
+
+        IEnumerable<SemVersion> CheckForMissingMajorReleases()
+        {
+            var latestRelease = ReleasesDescending.FirstOrDefault();
+            if (latestRelease == null) yield break;
+            for (var major = 1; major <= latestRelease.Version.Major; major++)
+            {
+                var version = new SemVersion(major, 0, 0);
+                if (FindRelease(version) == null)
+                {
+                    yield return version;
+                }
+            }
+        }
+
+
+        IEnumerable<SemVersion> CheckForMissingMinorReleases()
+        {
+            var latestMajorSeriesReleases =
+                ReleasesAscending
+                    .GroupBy(r => r.Version.Change(minor: 0, patch: 0))
+                    .Select(g => g.OrderBy(r => r.Version).Last())
+                    .OrderBy(r => r.Version);
+
+            foreach (var latestRelease in latestMajorSeriesReleases)
+            {
+                for (var minor = 1; minor <= latestRelease.Version.Minor; minor++)
+                {
+                    var minorVersion = latestRelease.Version.Change(minor: minor, patch: 0);
+                    if (FindRelease(minorVersion) == null)
+                    {
+                        yield return minorVersion;
+                    }
+                }
+            }
+        }
+
+
+        IEnumerable<SemVersion> CheckForMissingPatchReleases()
+        {
+            var latestMinorSeriesReleases =
+                ReleasesAscending
+                    .GroupBy(r => r.Version.Change(patch: 0))
+                    .Select(g => g.OrderBy(r => r.Version).Last())
+                    .OrderBy(r => r.Version);
+
+            foreach (var latestRelease in latestMinorSeriesReleases)
+            {
+                for (var patch = 1; patch < latestRelease.Version.Patch; patch++)
+                {
+                    var patchVersion = latestRelease.Version.Change(patch: patch);
+                    if (FindRelease(patchVersion) == null)
+                    {
+                        yield return patchVersion;
+                    }
+                }
             }
         }
 
@@ -336,8 +154,10 @@ namespace Verbot
             var passed =
                 ReleasesAscending.Aggregate(true, (result, release) =>
                     result &=
-                        release.IsMajor ? CheckMajorReleaseLineage(release)
-                        : release.IsMinor ? CheckMinorReleaseLineage(release)
+                        release.IsMajor
+                            ? CheckMajorReleaseLineage(release)
+                        : release.IsMinor
+                            ? CheckMinorReleaseLineage(release)
                         : CheckPatchReleaseLineage(release));
 
             if (!passed)
--- Verbot/VerbotRepository.Old.cs
+++ Verbot/VerbotRepository.Old.cs
@@ -177,5 +177,205 @@ namespace Verbot
                     v.Change(null, null, v.Patch + 1, null, null);
         }
 
+
+        void CheckForVersionLocations()
+        {
+            var locations = FindVersionLocations();
+
+            if (locations.Count == 0)
+            {
+                throw new UserException("No locations found in repository to record version");
+            }
+        }
+
+
+        void CheckForConflictingVersions()
+        {
+            var locations = FindVersionLocations();
+
+            var distinctVersions =
+                locations
+                    .Select(location => location.GetVersion())
+                    .Where(version => version != null)
+                    .Distinct()
+                    .ToList();
+
+            if (distinctVersions.Count == 1)
+            {
+                return;
+            }
+
+            Trace.TraceError("Conflicting versions found in repository:");
+            foreach (var location in locations)
+            {
+                var version = location.GetVersion() ?? "(none)";
+                var description = location.Description;
+                Trace.TraceError($"  {version} in {description}");
+            }
+            Trace.TraceError("Consider re-setting the correct current version using the 'set' command.");
+
+            throw new UserException("Conflicting versions found in repository");
+        }
+
+
+        void CheckForMissingVersions()
+        {
+            var locations = FindVersionLocations();
+            var missingLocations =
+                locations
+                    .Where(location => location.GetVersion() == null)
+                    .ToList();
+
+            if (missingLocations.Count == 0)
+            {
+                return;
+            }
+
+            Trace.TraceWarning($"Missing versions in some location(s) in the repository:");
+            foreach (var location in missingLocations)
+            {
+                Trace.TraceWarning($"  {location.Description}");
+            }
+            Trace.TraceWarning("Consider re-setting the current version using the 'set' command.");
+        }
+
+
+        void CheckVersionHasNotBeenReleased()
+        {
+            var releaseVersion = ReadFromVersionLocations().Change(null, null, null, "", "");
+            if (GitRepository.GetTags().Any(t => t.Name == releaseVersion))
+                throw new UserException("Current version has already been released");
+        }
+
+
+        void CheckVersionIsMasterPrerelease()
+        {
+            var version = ReadFromVersionLocations();
+            if (version.Prerelease != "master")
+                throw new UserException("Expected current version to be a -master prerelease");
+        }
+
+
+        void CheckVersionIsReleaseOrMasterPrerelease()
+        {
+            var version = ReadFromVersionLocations();
+            if (!(version.Prerelease == "" || version.Prerelease == "master"))
+                throw new UserException("Expected current version to be a release or -master prerelease");
+        }
+
+
+        void CheckMasterBranchIsTrackingHighestVersion()
+        {
+            if (MasterBranches.Any(mb => mb.Name == "master") && MasterBranches.First().Name != "master")
+                throw new UserException("Expected master branch to be tracking the latest version");
+        }
+
+
+        void CheckOnCorrectMasterBranchForVersion()
+        {
+            var minorVersion = ReadFromVersionLocations().Change(null, null, 0, "", "");
+            var expectedCurrentBranch =
+                MasterBranches
+                    .Where(mb => mb.Version == minorVersion)
+                    .Select(mb => mb.Name)
+                    .SingleOrDefault();
+            if (expectedCurrentBranch == null)
+                throw new UserException("No master branch found for current version");
+            if (GitRepository.GetBranch() != expectedCurrentBranch)
+                throw new UserException("Expected to be on branch " + expectedCurrentBranch);
+        }
+
+
+        void CheckNotSkippingRelease(bool major, bool minor)
+        {
+            var patch = !(major || minor);
+            var version = ReadFromVersionLocations();
+            if (version.Prerelease != "master") return;
+            var patchString = FormattableString.Invariant($"{version.Major}.{version.Minor}.{version.Patch}");
+            var minorString = FormattableString.Invariant($"{version.Major}.{version.Minor}");
+            if (patch)
+                throw new UserException(FormattableString.Invariant(
+                    $"No need to increment patch when {patchString} hasn't been released yet"));
+            if (minor && version.Patch == 0)
+                throw new UserException(FormattableString.Invariant(
+                    $"No need to increment minor when {patchString} hasn't been released yet"));
+            if (major && version.Minor == 0 && version.Patch == 0)
+                throw new UserException(FormattableString.Invariant(
+                    $"No need to increment major when {minorString} hasn't been released yet"));
+        }
+
+
+        void CheckNotAdvancingToLatestVersionOnNonMasterBranch(SemVersion newVersion)
+        {
+            if (!MasterBranches.Any()) return;
+            var newMinorVersion = newVersion.Change(null, null, 0, "", "");
+            if (newMinorVersion > MasterBranches.First().Version && GitRepository.GetBranch() != "master")
+                throw new UserException("Must be on master branch to advance to latest version");
+        }
+
+
+        void CheckForIncorrectRemoteTags()
+        {
+            var verbotTagsWithRemote = FindReleaseTagsWithRemote();
+
+            var incorrectRemoteTags =
+                verbotTagsWithRemote
+                    .Where(t => t.RemoteTarget != null)
+                    .Where(t => t.RemoteTarget != t.Target)
+                    .ToList();
+
+            if (!incorrectRemoteTags.Any()) return;
+
+            foreach (var tag in incorrectRemoteTags)
+            {
+                Trace.TraceError($"Remote tag {tag.Name} at {tag.RemoteTarget} local {tag.Target}");
+            }
+
+            throw new UserException("Incorrect remote tag(s) found");
+        }
+
+
+        void CheckForRemoteBranchesAtUnknownCommits()
+        {
+            var verbotBranchesWithRemote = GetVerbotBranchesWithRemote();
+
+            var remoteBranchesAtUnknownCommits =
+                verbotBranchesWithRemote
+                    .Where(b => b.RemoteTarget != null)
+                    .Where(b => !GitRepository.Exists(b.RemoteTarget))
+                    .ToList();
+
+            if (!remoteBranchesAtUnknownCommits.Any()) return;
+
+            foreach (var branch in remoteBranchesAtUnknownCommits)
+            {
+                Trace.TraceError($"Remote branch {branch.Name} at unknown commit {branch.RemoteTarget}");
+            }
+
+            throw new UserException("Remote branch(es) at unknown commits");
+        }
+
+
+        void CheckForRemoteBranchesNotBehindLocal()
+        {
+            var verbotBranchesWithRemote = GetVerbotBranchesWithRemote();
+
+            var remoteBranchesNotBehindLocal =
+                verbotBranchesWithRemote
+                    .Where(b => b.RemoteTarget != null)
+                    .Where(b => !GitRepository.IsAncestor(b.RemoteTarget, b.Target))
+                    .ToList();
+
+            if (!remoteBranchesNotBehindLocal.Any()) return;
+
+            foreach (var branch in remoteBranchesNotBehindLocal)
+            {
+                Trace.TraceError(
+                    $"Remote branch {branch.Name} at {branch.RemoteTarget} not behind local at {branch.Target}");
+            }
+
+            throw new UserException("Remote branch(es) not behind local");
+        }
+
     }
 }
